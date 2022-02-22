63e793ac6c411dd8e2ab7ac0b4b681f862162129
Josue Pihuala <josuepihuala@Josues-Mac-mini.local>
2022-02-21 22:54:41 -0300

    entrega2

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar= () => {
+    return(
+        <div>
+            <ul >
+                <li><a href="./index.html">Inicio</a></li>
+                <li><a href="">Productos</a>
+                    <ul>
+                    <li><a href="./views/shampoo.html">Shampoo </a></li>
+                    <li><a href="./404.html">Serum</a></li>
+                    </ul>
+                </li>    
+                <li><a href="https://www.instagram.com/chaupootienda/" target="_blank">Instagram</a> </li>
+                <li><a href="https://www.facebook.com/chaupootienda/" target="_blank">Facebook</a></li>
+                <li><a href="./views/tienda.html">Tienda</a></li>
+                <li><a href="./views/contacto.html">Contacto</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export { NavBar }
